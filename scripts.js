12c76fbf139188927039db6ba84742ee69e5fb8b
Bebi <64404343+Bebe-ops@users.noreply.github.com>
2020-05-01 11:36:37 +0200

    add some js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
+// Third part
 
-$('h1').text("This is..."JavaScript" );
-$('p').css("border","1px solid red");
+//$('h1').text('This is done by <em>JavaScript-text</em>');                         //  $('css selector').modify('action');
+$('h1').html('This is done by <em>Javasript-html</em>');
+$('h2').toggleClass('blue');			//on the class blue	
+$('h2').toggleClass('blue');			//of the class blue
 
 
-console.log("Sript file is loaded");
+
+function myLittleFunction() {								//function name() {any kind of javascript commands}
+	$('main').append('<p>A p is appended in the main</p>');
+	$('p').css('border', '1px solid grey');
+	$('p').css('padding', '10px');
+}
+
+myLittleFunction();									//function use	
+myLittleFunction();									//repeat function
+myLittleFunction();									//repeat function
+
+//   $('#add').click(here is the function);  
+$('#add').click(myLittleFunction);						//if you click-->add paragraph
+$('#remove').click(function() {							//if you click-->remove paragraph
+$('p:last-child').remove();								//$('p').remove(); --> remove all paragraph.
+});
+
+//First part
+console.log("Sript file is loaded.");
+
+
+//Single line comment
+/*Multiple 
+lines
+of comments*/
