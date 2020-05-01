31617295c7b075a2072f2574bdf54a7a8a174c69
Bebi <64404343+Bebe-ops@users.noreply.github.com>
2020-05-01 20:12:58 +0200

    add some example

--- scripts.js
+++ scripts.js
@@ -5,26 +5,65 @@ $('h1').html('This is done by <em>Javasript-html</em>');
 $('h2').toggleClass('blue');			//on the class blue	
 $('h2').toggleClass('blue');			//of the class blue
 
+myLittleFunction();									//function use	
+myLittleFunction();									// repeat function
+myLittleFunction();									// repeat function
 
-
-function myLittleFunction() {								//function name() {any kind of javascript commands}
-	$('main').append('<p>A p is appended in the main</p>');
+function myLittleFunction() {							// function name() {any kind of javascript commands}
+	//	condition:number of paragraphs smaller than 5
+	if ($('p').length< 5) {
+		$('main').append('<p>A p is appended in the main</p>');
+	} else {
+		console.log('We reached the maximum paragraph number');
+	}
+	
 	$('p').css('border', '1px solid grey');
 	$('p').css('padding', '10px');
 }
 
-myLittleFunction();									//function use	
-myLittleFunction();									//repeat function
-myLittleFunction();									//repeat function
-
-//   $('#add').click(here is the function);  
-$('#add').click(myLittleFunction);						//if you click-->add paragraph
-$('#remove').click(function() {							//if you click-->remove paragraph
-$('p:last-child').remove();								//$('p').remove(); --> remove all paragraph.
+// $('#add').click(here is the function);  
+$('#add').click(myLittleFunction);							// if you click-->add paragraph
+$('#remove').click(function () {							// if you click-->remove paragraph
+	$('p:last-child').remove();								// $('p').remove(); --> remove all paragraph.
 });
 
+// Fourth part
+
+// if (condition that true or false) {
+	//condition is true
+//} else {
+	//condition is false
+//} 
+
+
+let colors = ['orange', 'yellow', 'blue', 'lime', 'magenta'];
+
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[0]);			//colors[0]='orange'
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[1]);	
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[2]);	
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[3]);
+// $('#box-container').append('<div class="box"></div>');
+// $('.box:last-child').css('background', colors[4]);
+
+colors.forEach(function (color) {
+	$('#box-container').append('<div class="box"></div>');
+	$('.box:last-child').css('background', color);
+});
+
+//for (let index = 0; index < array.length; index++) {
+//	const element = array[index];	
+//}
+
+for (let i = 0; i < 20; i++) {							//i=i+1  =  i++
+	console.log('loki');
+}
+
 //First part
-console.log("Sript file is loaded.");
+console.log("Sript file is loaded.");				//write in display
 
 
 //Single line comment
